13c530e5589037be8c7dbbe14f7644c21fea6cd2
PRO <pro@n-side.com>
2024-12-09 17:06:51 +0100

    day1 part1

--- days/day1/day1.ts
+++ days/day1/day1.ts
@@ -8,9 +8,26 @@ const filePath = path.join(__dirname, 'input.txt');
 const fileContent: string = fs.readFileSync(filePath, 'utf8');
 
 // Process the file content
-const dataArray: number[][] = fileContent
+const locations: number[][] = fileContent
   .trim()
   .split('\n')
   .map(line => line.split(/\s+/).map(Number));
 
-console.log(dataArray);
+let locations1: number[] = [];
+let locations2: number[] = [];
+
+locations.map(location => {
+  locations1.push(location[0]);
+  locations2.push(location[1]);
+});
+
+locations1.sort();
+locations2.sort();
+
+const locationsDiff = locations1.map((location, index) => {
+  return Math.abs(locations1[index] - locations2[index]);
+});
+
+const totalLocationsDiff = locationsDiff.reduce((acc, curr) => acc + curr, 0);
+
+console.log(totalLocationsDiff);
