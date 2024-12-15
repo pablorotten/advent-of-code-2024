82279e6a13204b0a2086c499d653048b05b7ea4d
PRO <pro@n-side.com>
2024-12-15 17:49:07 +0100

    Day 7 part 1

---
+++ days/day7/day7p1.ts
@@ -0,0 +1,63 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const filePath = path.join(__dirname, "input.txt");
+const input: string = fs.readFileSync(filePath, "utf8");
+const equations = input
+  .split("\n")
+  .map((row) => row.replace(":", "").split(" "));
+const operators = ["+", "*"];
+
+function generateCombinations<T>(elements: T[], n: number): T[][] {
+  const result: T[][] = [];
+
+  function helper(current: T[]) {
+    // Base case: if the current combination's length matches n, add to results
+    if (current.length === n) {
+      result.push([...current]);
+      return;
+    }
+
+    // Recursive case: append each element and recurse
+    for (const element of elements) {
+      current.push(element);
+      helper(current);
+      current.pop(); // Backtrack
+    }
+  }
+
+  helper([]); // Start recursion with an empty array
+  return result;
+}
+
+let totalCalibrationResult = 0;
+equations.map((equation) => {
+  // equation -> 3267: 81 40 27
+  const [result, ...parameters] = equation.map((term) => Number(term));
+  const numberOfOperators = parameters.length - 1;
+  // possibleOperators -> [  [ '+', '+', '+' ],  [ '+', '+', '*' ],  [ '+', '*', '+' ], ... ]
+  const possibleOperations = generateCombinations(operators, numberOfOperators);
+  let currentTotalCalibration = 0;
+
+  // take one operation from possibleOperators -> [  [ '+', '+', '+' ],  [ '+', '+', '*' ],  [ '+', '*', '+' ], ... ]
+  for (const operation of possibleOperations) {
+    // calculate the result
+    const operationResult = operation.reduce((acc, operator, operatorIndex) => {
+      switch (operator) {
+        case "+":
+          if (operatorIndex === 0) return parameters[0] + parameters[1];
+          else return acc + parameters[operatorIndex + 1];
+        case "*":
+          if (operatorIndex === 0) return parameters[0] * parameters[1];
+          else return acc * parameters[operatorIndex + 1];
+      }
+    }, 0);
+
+    if (operationResult === result) {
+      totalCalibrationResult += operationResult;
+      break;
+    }
+  }
+});
+
+console.log(totalCalibrationResult);
